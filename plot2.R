fadf6f8c6c209c5b29bb6b297021ba9f68f0c95e
MLS <mlshore@yahoo.com>
2025-11-28 16:22:19 -0900

    correct overwriting plot2 with plot3

--- plot2.R
+++ plot2.R
@@ -6,11 +6,7 @@ rm(tbl)                     # free up some memory
 times <- paste(dat$Date, dat$Time) %>% strptime(format="%d/%m/%Y %H:%M:%S")
 
 # send the plot to an output file, do the plot, close the file
-png(filename = './plot3.png', width = 480, height = 480, units = 'px')
-plot(times, dat$Sub_metering_1, type='l', xlab="", 
+png(filename = './plot2.png', width = 480, height = 480, units = 'px')
+plot(times, dat$Global_active_power, type='l', xlab="", 
      ylab='Global Active Power (kilowatts)')
-lines(times, dat$Sub_metering_2, col="red")
-lines(times, dat$Sub_metering_3, col='blue')
-legend("topright", pch=1, col=c('black', 'red', 'blue'), 
-       legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
-dev.off()
+dev.off
