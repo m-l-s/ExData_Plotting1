efbeba38d85ee78dc52ab3f5cd711b552c49c348
MLS <mlshore@yahoo.com>
2025-11-29 11:51:20 -0900

    fixed wrong plot upload

--- plot1.R
+++ plot1.R
@@ -9,5 +9,5 @@ dat$Global_active_power <- as.numeric((dat$Global_active_power))
 
 png(filename='./plot1.png', width=480, height=480, units='px')
 hist(dat$Global_active_power, col='red', main='Global Active Power', 
-     xlab='Global Active Power (kilowatts)')
+     xlab='Global Active Power (Kilowatts)')
 dev.off()
